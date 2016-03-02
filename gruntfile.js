6f7c31744350b62ad7666bf443a98dbca17dc6a0
Jonas Päckos <jpackos@gmail.com>
2016-03-02 10:31:04 +0100

    Added Sass (Libsass) to the grunt workflow
    Sass files are compiled to CSS in Pattern Lab, then copied over to the
    Jekyll site when built.

--- gruntfile.js
+++ gruntfile.js
@@ -49,6 +49,114 @@ module.exports = function(grunt) {
       }
     },
 
+
+
+
+
+    /**
+     *
+     * Compile Sass to CSS
+     *
+     */
+
+     sass: {
+
+       styles: {
+         options: {
+           sourcemap: 'none',
+           outputStyle: 'expanded',
+           lineComments: false,
+           debugInfo: false,
+           noCache: true
+         },
+
+         files: [{
+           expand: true,
+           cwd: 'patterns/source/css/scss',
+           src: ['*.scss'],
+           dest: 'patterns/source/css',
+           ext: '.css'
+         }]
+       },
+
+       minified: {
+         options: {
+           sourcemap: 'none',
+           outputStyle: 'compressed',
+           lineComments: false,
+           debugInfo: false,
+           noCache: true
+         },
+
+         files: [{
+           expand: true,
+           cwd: 'patterns/source/css/scss',
+           src: ['*.min.scss'],
+           dest: 'patterns/source/css',
+           ext: '.min.css'
+         }]
+       }
+
+     },
+
+
+
+
+
+     /**
+      *
+      * Copy asset files to Jekyll
+      *
+      */
+
+     copy: {
+       css: {
+         expand: true,
+         cwd: 'patterns/source/css',
+         src: '**/*.min.css',
+         dest: 'css'
+       },
+
+       gfx: {
+         expand: true,
+         cwd: 'patterns/source/css/gfx',
+         src: '**/*',
+         dest: 'css/gfx'
+       },
+
+       type: {
+         expand: true,
+         cwd: 'patterns/source/css/type',
+         src: '**/*',
+         dest: 'css/type'
+       }
+     },
+
+
+
+
+
+      /**
+       *
+       * Notify task progress to developer
+       *
+       */
+
+      notify: {
+        done: {
+          options: {
+            title: 'pekkos.com: Done',
+            message: 'OK, I did my stuff for you.'
+          }
+        },
+        sass: {
+          options: {
+            title: 'pekkos.com: Working on Sass files',
+            message: 'Sass files are being compiled to CSS'
+          }
+        }
+      }
+
   });
 
 
@@ -60,8 +168,14 @@ module.exports = function(grunt) {
    *
    */
 
+  // Scaffolding packages
   grunt.loadNpmTasks('grunt-dev-update');
   grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-notify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // Real stuff :)
+  grunt.loadNpmTasks('grunt-sass');
 
 
 
@@ -73,10 +187,18 @@ module.exports = function(grunt) {
    *
    */
 
+  // Default task
   grunt.registerTask('default', ['build']);
   grunt.registerTask('build', ['shell:weather', 'devUpdate']);
-  grunt.registerTask('jekyll', ['shell:weather', 'shell:jekyll_build']);
+
+  // Build Jekyll site using assets from Pattern Lab
+  grunt.registerTask('jekyll', ['copy', 'shell:jekyll_build']);
+
+  // Update packages
   grunt.registerTask('update', ['devUpdate']);
+
+  // Compile Sass to CSS
+  grunt.registerTask('css', ['sass', 'notify:done']);
 
 
 };
