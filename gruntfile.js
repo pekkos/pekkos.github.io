37bc8792470fa221266b078aec9a5d8b8f15bfee
Jonas Päckos <jpackos@gmail.com>
2017-01-18 21:49:17 +0100

    Added a few grunt packages

--- gruntfile.js
+++ gruntfile.js
@@ -41,19 +41,25 @@ module.exports = function(grunt) {
      */
 
     shell: {
+
       // Show current weather
       weather: {
         command: "curl -s http://wttr.in/Gothenburg | head -7"
       },
-      // Build patterns @ Pattern Lab
+
+      // Generate patterns @ Pattern Lab
       patternlab_build: {
         command: "php patterns/core/console --generate"
       },
-      jekyll_build: {
-        command: "jekyll build --limit_posts 10"
+
+      // Generate Jekyll site with only 10 posts
+      jekyll_build_limited: {
+        command: "bundle exec jekyll build --limit_posts 10"
       },
+
+      // Generate full Jekyll site
       jekyll_build_full: {
-        command: "jekyll build"
+        command: "bundle exec jekyll build"
       }
     },
 
@@ -87,24 +93,93 @@ module.exports = function(grunt) {
          }]
        },
 
-       minified: {
-         options: {
-           sourcemap: 'none',
-           outputStyle: 'compressed',
-           lineComments: false,
-           debugInfo: false,
-           noCache: true
-         },
+      //  minified: {
+      //    options: {
+      //      sourcemap: 'none',
+      //      outputStyle: 'compressed',
+      //      lineComments: false,
+      //      debugInfo: false,
+      //      noCache: true
+      //    },
+       //
+      //    files: [{
+      //      expand: true,
+      //      cwd: 'patterns/source/css/scss',
+      //      src: ['*.min.scss'],
+      //      dest: 'patterns/source/css',
+      //      ext: '.min.css'
+      //    }]
+      //  }
 
+     },
+
+
+
+
+
+     /**
+      *
+      * Process CSS with PostCSS plugins
+      *
+      */
+
+     postcss: {
+       files: {
+         options: {
+           map: false, // inline sourcemaps
+
+           processors: [
+             require('postcss-merge-rules'), // merge adjacent css rules
+             require('pixrem')(), // add fallbacks for rem units
+             require('autoprefixer')({browsers: ['last 3 versions', 'ie 8', 'ie 9', '> 1%']}) // add vendor prefixes
+           ]
+         },
+         src: 'patterns/source/css/**/*.css'
+       }
+     },
+
+
+
+
+
+     /**
+      *
+      * Minify CSS
+      *
+      */
+
+     cssmin: {
+       css:{
          files: [{
            expand: true,
-           cwd: 'patterns/source/css/scss',
-           src: ['*.min.scss'],
+           cwd: 'patterns/source/css',
+           src: ['*.css', '!pattern-scaffolding.css'],
            dest: 'patterns/source/css',
+           flatten: true,
            ext: '.min.css'
          }]
        }
+     },
 
+
+
+
+
+     /**
+      *
+      * Gzip
+      * Calculates Gzipped CSS file sizes
+      *
+      */
+
+     gzip: {
+       options: { detail: true },
+       files: {
+         // Target-specific file lists and/or options go here.
+         src: [
+           'patterns/source/css/styles.min.css'
+         ]
+       },
      },
 
 
@@ -163,7 +238,55 @@ module.exports = function(grunt) {
             message: 'Sass files are being compiled to CSS'
           }
         }
+      },
+
+
+
+
+
+    /**
+     *
+     * Watch for changes
+     *
+     */
+
+    watch: {
+
+      // Watch to see if we change this gruntfile
+
+      gruntfile: {
+        files: ['gruntfile.js']
+      },
+
+
+
+      // Live Reload
+
+      options: {
+        livereload: true,
+      },
+
+
+
+      // Compile to CSS when changes occur in Sass files
+
+      css: {
+        files: [
+          'patterns/source/css/scss/*.scss',
+          'patterns/source/css/scss/**/*.scss'
+        ],
+        tasks: [
+          'sass',           // compile Sass to CSS
+          'newer:postcss'   // post process CSS
+        ],
+        options: {
+          spawn: false
+        }
       }
+
+    }
+
+
 
   });
 
@@ -181,9 +304,15 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-notify');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-newer');
+  grunt.loadNpmTasks('grunt-gzip');
 
   // Real stuff :)
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
 
 
 
@@ -195,24 +324,52 @@ module.exports = function(grunt) {
    *
    */
 
-  // Default task
+  /* Default task */
+
   grunt.registerTask('default', ['build']);
-  grunt.registerTask('build', ['shell:weather', 'devUpdate']);
-
-  // Build Patterns in Pattern Lab
-  grunt.registerTask('pl', ['shell:patternlab_build']);
-
-  // Build Jekyll site using assets from Pattern Lab
-  grunt.registerTask('jekyll', ['copy', 'shell:jekyll_build']);
-
-  // Build full Jekyll site using assets from Pattern Lab
-  grunt.registerTask('jekyll_full', ['copy', 'shell:jekyll_build_full']);
+  grunt.registerTask('build', ['shell:weather', 'devUpdate', 'watch']);
 
   // Update packages
   grunt.registerTask('update', ['devUpdate']);
 
-  // Compile Sass to CSS
-  grunt.registerTask('css', ['sass', 'notify:done']);
 
 
+  /* Pattern Lab */
+
+  // Build Patterns in Pattern Lab
+  grunt.registerTask('pl', ['shell:patternlab_build']);
+
+
+
+  /* Jekyll */
+
+  // Build Jekyll site using 'grunt j'
+  grunt.registerTask('j', ['shell:jekyll_build_full']);
+
+  // Build limited Jekyll site using 'grunt j10'
+  grunt.registerTask('j', ['shell:jekyll_build_limited']);
+
+  // Build Jekyll site with updated assets from Pattern Lab using 'grunt jekyll'
+  grunt.registerTask('jekyll', ['copy', 'shell:jekyll_build_full']);
+
+
+
+
+  /* CSS tasks */
+
+  // Compile Sass to CSS using 'grunt scss'
+  grunt.registerTask('scss', ['sass', 'notify:done']);
+
+  // Run PostCSS processes on CSS with 'grunt post'
+  grunt.registerTask('post', ['postcss', 'notify:done']);
+
+  // Minify CSS files using 'grunt min'
+  grunt.registerTask('min', ['cssmin', 'gzip', 'notify:done']);
+
+  // Run complete CSS workflow with 'grunt css'
+  grunt.registerTask('css', ['sass', 'postcss', 'cssmin', 'notify:done']);
+
+
+
+  // End of file.
 };
